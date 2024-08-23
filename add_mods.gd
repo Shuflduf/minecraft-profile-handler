00b664531f3ba67df25f08dbb7336aec063a7b1c
Shuflduf <shuflduf@gmail.com>
2024-08-22 21:09:18 -0600

    select file dialog

--- add_mods.gd
+++ add_mods.gd
@@ -1,8 +1,6 @@
 extends Window
 
 
-
-
 func _on_close_requested() -> void:
 	hide()
 
@@ -11,6 +9,17 @@ func _on_files_dropped(files: PackedStringArray) -> void:
 	if $TabContainer.current_tab != 0:
 		return
 	for i in files:
+		DirAccess.copy_absolute(i, \
+				get_parent().get_mods_path().strip_edges() + "/" + i.get_file())
+	get_parent().pressed.emit()
+
+
+func _on_select_button_pressed() -> void:
+	$TabContainer/Select/FileDialog.show()
+
+
+func _on_file_dialog_files_selected(paths: PackedStringArray) -> void:
+	for i in paths:
 		DirAccess.copy_absolute(i, \
 				get_parent().get_mods_path().strip_edges() + "/" + i.get_file())
 	get_parent().pressed.emit()
